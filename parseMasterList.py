26d6c3bfcadcdcace324747da19b564932805ae6
klitscher <672@holbertonschool.com>
2019-10-10 20:14:23 +0000

    Adding file to parse the master list

---
+++ parseMasterList.py
@@ -0,0 +1,25 @@
+#!usr/bin/env python3
+"""Module that parses list of Albums and moods"""
+
+def parseMasterList(filePath):
+    """Method to parse the album text file
+
+    filePath: Path to the text file
+
+    """
+    albums = {}
+    tracks = {}
+    with open(filePath) as f:
+        albumName = None
+        for line in f:
+            if albumName is None:
+                albumName = line.rstrip('\n')
+                albums[albumName] = {}
+                tracks = albums[albumName]
+            elif line == '%\n':
+                albumName = None
+            else:
+                track = line.partition(' ')[0]
+                moods = set(line.partition(' ')[2].rstrip('\n').split(', '))
+                tracks[track] = moods
+    return albums
