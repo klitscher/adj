745933d98a2c0c9521291eb356a46d1a09e9e1b0
HermesBoots <samuel.of.hermes@gmail.com>
2019-10-11 06:29:10 +0000

    Add package file with 2 utility attributes.
    path: absolute path to the package
    
    platform: operating system and word size of the executing platform

---
+++ __init__.py
@@ -0,0 +1,35 @@
+"""Atmospheric DJ: play songs fitting your mood
+
+Longer description
+
+"""
+
+
+import collections as _collections
+import sys as _sys
+
+
+_Platform = _collections.namedtuple('Platform', ('os', 'bits'))
+
+
+def _getPlatform() -> _Platform:
+    """Get the operating system and word size of the computer."""
+    if _sys.platform.startswith('cygwin'):
+        os = 'cygwin'
+    elif _sys.platform.startswith('darwin'):
+        os = 'mac'
+    elif _sys.platform.startswith('win32'):
+        os = 'windows'
+    else:
+        os = 'unix'
+    if _sys.maxsize == 0x7FFFFFFF:
+        bits = 32
+    elif _sys.maxsize == 0x7FFFFFFFFFFFFFFF:
+        bits = 64
+    else:
+        bits = -1
+    return _Platform(os, bits)
+
+
+path = __path__[0]
+platform = _getPlatform()
