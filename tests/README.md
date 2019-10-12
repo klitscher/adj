35d9de94985cf69b221f49c1a4ad16a6bbb53d95
HermesBoots <samuel.of.hermes@gmail.com>
2019-10-12 21:46:25 +0000

    Expand on audio system tests.

--- tests/README.md
+++ tests/README.md
@@ -7,5 +7,10 @@ Typically, with the source code installed in a directory called "adj" while you'
 python3 -m unittest discover adj
 ```
 
+## Manual Tests
+Normally, when you run the test suite, all the tests run automatically and you're given a report of which pass and which fail. However, some tests require feedback from the tester and can't be run automatically. Running these tests requires extra steps.
+
+To run the manual audio tests that ask you whether you can hear the music, set the environment variable `ADJ_DEVICE` to the index of your preferred sound device (to use the default one, set it to -1).
+
 ## Credits
 The sample audio files come from [file-examples.com](https://file-examples.com/index.php/sample-audio-files/).
--- tests/test_audio.py
+++ tests/test_audio.py
@@ -1,7 +1,10 @@
 import adj
 import adj.audio
+import gc
 import os
 import os.path
+import threading
+import time
 import unittest
 
 
@@ -20,6 +23,7 @@ class AudioTests (unittest.TestCase):
     )
     def testAudible(self):
         """Try to play sound and prompt the tester if they hear it."""
+        song = None
         for extension in ('mp3', 'ogg'):
             with self.subTest(extension):
                 path = os.path.join(adj.path, 'tests', 'sample.' + extension)
@@ -30,3 +34,89 @@ class AudioTests (unittest.TestCase):
                     answer = input('Enter just "y" or "n".')
                 song.stop()
                 self.assertEqual(answer, 'y')
+        if isinstance(song, adj.audio.Music): song.stop()
+
+    def testPlayPause(self):
+        """Play and pause the song, making sure it resumes."""
+        song = None
+        for extension in ('mp3', 'ogg'):
+            with self.subTest(extension):
+                path = os.path.join(adj.path, 'tests', 'sample.' + extension)
+                song = adj.audio.Music(path)
+                song.play()
+                time.sleep(.2)
+                pos = adj.audio.bass.BASS_ChannelGetPosition(
+                    song._handle,
+                    adj.audio.PositionFlags.BYTE
+                )
+                self.assertGreater(pos, 0)
+                song.pause()
+                self.assertFalse(song.playing)
+                song.play()
+                time.sleep(.1)
+                self.assertLess(pos, adj.audio.bass.BASS_ChannelGetPosition(
+                    song._handle,
+                    adj.audio.PositionFlags.BYTE
+                ))
+                self.assertTrue(song.playing)
+                song.stop()
+                self.assertFalse(song.playing)
+                with self.assertRaises(TypeError):
+                    song.play()
+        if isinstance(song, adj.audio.Music): song.stop()
+
+    def testSongFreed(self):
+        """Ensure the song's resources are freed as intended."""
+        song = None
+        for extension, length in (('mp3', 6961528), ('ogg', 19033400)):
+            with self.subTest(extension):
+                path = os.path.join(adj.path, 'tests', 'sample.' + extension)
+                with self.subTest('free on end'):
+                    song = adj.audio.Music(path)
+                    lock = threading.Lock()
+                    lock.acquire()
+                    @adj.audio.SyncCallback
+                    def callback(a, b, c, d):
+                        lock.release()
+                    adj.audio.bass.BASS_ChannelSetSync(
+                        song._handle,
+                        adj.audio.SyncFlags.FREE,
+                        0,
+                        callback,
+                        None
+                    )
+                    song.play()
+                    adj.audio.bass.BASS_ChannelSetPosition(
+                        song._handle,
+                        length - 1,
+                        (
+                            adj.audio.PositionFlags.BYTE |
+                            adj.audio.PositionFlags.SCAN |
+                            adj.audio.PositionFlags.INEXACT
+                        )
+                    )
+                    self.assertTrue(lock.acquire(timeout=2))
+                with self.subTest('free on stop'):
+                    song = adj.audio.Music(path)
+                    song.stop()
+                    adj.audio.bass.BASS_ChannelGetLength(song._handle, 0)
+                    self.assertEqual(
+                        adj.audio.bass.BASS_ErrorGetCode(),
+                        adj.audio.Errors.HANDLE
+                    )
+                if isinstance(song, adj.audio.Music): song.stop()
+                with self.subTest('free on garbage collection'):
+                    song = adj.audio.Music(path)
+                    handle = song._handle
+                    del song
+                    gc.collect()
+                    adj.audio.bass.BASS_ChannelGetLength(handle, 0)
+                    self.assertEqual(
+                        adj.audio.bass.BASS_ErrorGetCode(),
+                        adj.audio.Errors.HANDLE
+                    )
+
+    def testUnicode(self):
+        """Ensure audio files with Unicode names can play."""
+        song = os.path.join(adj.path, 'tests', '앢⓫䖴ᦓᙴ.mp3')
+        song = adj.audio.Music(song)
