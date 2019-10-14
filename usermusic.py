8cf1a028b21e2849ad8642941e57f31c5e2a4b98
rmf10003 <685@holbertonschool.com>
2019-10-14 21:59:29 +0000

    check if user song is located in our master list.  If so than add song to song table and build association between its associated moods and itself in association table

---
+++ usermusic.py
@@ -0,0 +1,26 @@
+import adj
+import adj.audio
+import adj.db
+import adj.parseMasterList as parse
+import os
+
+
+def load_music(music_root, db_obj):
+    """parse user music and store matching items in db"""
+
+    albums = parse.parseMasterList(os.path.join(adj.path, 'allmoods.txt'))
+
+    for dirpath, dirname, filename in os.walk(music_root):
+        path = dirpath + filename
+        song = adj.audio.Music(path)
+        album = normalizeAlbum(song.metadata['album'])
+        if album in albums and song.metadata['track'] in albums[album]:
+            row = db_obj.insertMusicRow(
+                song.metadata['title'],
+                song.metadata['album'],
+                song.metadata['track'],
+                path
+            )
+            for mood in albums[album][song.metadata['track']]:
+                db_obj.insertAssociationRow(mood, row)
+    db_obj.commit()
