482c80c4ddc77db9b9c718edefae35d11c3ca1f6
Rory <fahyrory@gmail.com>
2019-10-22 15:38:49 -0400

    add recycleView of all songs in new filter and left justify result

--- gui/rightwidget.py
+++ gui/rightwidget.py
@@ -1,8 +1,26 @@
-import adj
+"""Module for right side of GUI"""
+import adj.playlist
+
 import kivy.lang
 import kivy.uix.boxlayout
-import os.path
+from kivy.uix.button import Button
+from kivy.uix.recycleview import RecycleView
 
 
 class RightWidget (kivy.uix.boxlayout.BoxLayout):
-    pass
+    """right side of gui class"""
+
+    
+    alt_playlist = None
+
+    
+    def on_start(self):
+        """dynamically create playlist view and render activate button"""
+        self.alt_playlist = self.parent.playlist
+        self.ids.alt_plst.data = [{'text': song.title} for song in self.alt_playlist]
+
+""" class RV(RecycleView):
+    def __init__(self, **kwargs):
+        super(RV, self).__init__(**kwargs)
+        self.data = [{'text': str(x) for x in range(100)}]
+ """
