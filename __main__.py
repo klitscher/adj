0c198cb806f08ce1c3c98a892a16da3f6fc88d67
HermesBoots <samuel.of.hermes@gmail.com>
2019-10-22 17:02:39 +0000

    Avoid blurriness on high DPI Windows screens.

--- __main__.py
+++ __main__.py
@@ -2,11 +2,15 @@ import adj
 import adj.audio
 import adj.gui.app
 import adj.gui.firstrun
+import ctypes
 import multiprocessing
 import os.path
 
+
 if adj.platform.os == 'mac':
     multiprocessing.set_start_method('spawn')
+elif adj.platform.os == 'windows':
+    ctypes.windll.shcore.SetProcessDpiAwareness(1)
 
 adj.audio.init()
 if not os.path.isfile(os.path.join(adj.path, 'adj.db')):
