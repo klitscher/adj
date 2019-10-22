d8b95a8c67e02edfce1c3b8075ed8f034d66e22b
Rory <fahyrory@gmail.com>
2019-10-22 16:50:39 -0400

    create event to have activate button switch playlist

--- gui/rightwidget.py
+++ gui/rightwidget.py
@@ -1,26 +1,37 @@
 """Module for right side of GUI"""
 import adj.playlist
 
+import functools
+
 import kivy.lang
 import kivy.uix.boxlayout
 from kivy.uix.button import Button
 from kivy.uix.recycleview import RecycleView
-
+from kivy.properties import ListProperty
 
 class RightWidget (kivy.uix.boxlayout.BoxLayout):
     """right side of gui class"""
 
+    song_list = ListProperty()
+            
+    def activate_playlist(self):
+        """call switchplaylist method of main widgit"""
+        playlist = adj.playlist.Playlist(self.song_list)
+        self.parent.switchPlaylist(playlist)
     
-    alt_playlist = None
+    def get_playlist(self, mood_dict):
+        """query the db for list of songs matching filter"""
+        if len(mood_dict) == 0:
+            self.song_list = []
+            return 
+        self.song_list = self.parent.db.filterMusic(mood_dict)
+        self.ids.alt_plst.data = [{'text': song.title} for song in self.song_list]
+        moods = functools.reduce(set.union, (song.moods for song in self.song_list), set())
+        return moods
 
-    
-    def on_start(self):
-        """dynamically create playlist view and render activate button"""
-        self.alt_playlist = self.parent.playlist
-        self.ids.alt_plst.data = [{'text': song.title} for song in self.alt_playlist]
-
-""" class RV(RecycleView):
-    def __init__(self, **kwargs):
-        super(RV, self).__init__(**kwargs)
-        self.data = [{'text': str(x) for x in range(100)}]
- """
+    def on_song_list(self, _self, value):
+        """enable button"""
+        if len(self.song_list) == 0:
+            self.ids.activate.disabled = True
+        else:
+            self.ids.activate.disabled = False
